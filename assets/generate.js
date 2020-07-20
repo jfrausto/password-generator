8ff1cabcf0117b030b9b70a29bae1f660fb6a7ba
jessef <1319957+jfrausto@users.noreply.github.com>
2020-07-19 23:22:37 -0700

    added specialChar prompt and atLeastOne validity checks

--- assets/generate.js
+++ assets/generate.js
@@ -1,8 +1,12 @@
 // Assignment Code
 // element references/pointers
 var generateBtn = document.querySelector("#generate");
-// maybe a list of vars that hold each preference
-
+// a list of vars that hold each preference
+var passwordLength;
+var specialPref;
+var lowerCasePref;
+var upperCasePref;
+var numericPref;
 // i think we are going to need a MASTER array containing everything we can randomize from.
 
 var masterArray = []; // holds the array of characters we can pick random from, according to preference
@@ -70,7 +74,8 @@ var specialChars = [
 // Write password to the #password input
 function writePassword() {
   // begin series of prompts
-  var prompts = displayPrompts(); //  will return boolean; true if ALL good INPUT; or undefined for canceled prompts
+  var prompts = displayPrompts();
+  //  will return boolean; true if ALL VALID INPUT; or undefined for canceled prompts
   console.log(prompts + " prompt validity");
 
   // if we did NOT CANCEL at any point, then generate the password!
@@ -89,7 +94,7 @@ function displayPrompts() {
   // while-loop for password length
   while (!validInput) {
     // ask for password length
-    var passwordLength = prompt(
+    passwordLength = prompt(
       "How many characters long do you want your password to be? (8-128)"
     );
     // if you press cancel, stop prompting
@@ -106,66 +111,101 @@ function displayPrompts() {
       alert("enter a number from 8-128");
     }
   }
-  //   put the while-loop here for preference check!!!!!!!!!
-  //   another while loop here for next valid check
-  //   REMEMBER WE NEED TO PICK AT LEAST ONE PREFERENCE!
-  validInput = false; // we start false until proven true!!
-  while (!validInput) {
-    var lowerCasePref = prompt(
-      "Do you want to include lower case letters? (y or n)"
-    );
-    if (lowerCasePref === null) {
-      //if you cancel, stop prompting
-      return;
-    }
-    console.log(lowerCasePref + " lowercase");
-    if (validChecker(lowerCasePref)) {
-      // check valid input
-      validInput = true;
-      if (lowerCasePref == "y") {
-        pickPref = true; // true only when they picked a preference
+
+  while (!pickPref) {
+    // this while-loop controls a re-prompt;
+    // need to pick at least one preference (or cancel) to break out of loop
+    validInput = false; // we start false until proven true!!
+    while (!validInput) {
+      lowerCasePref = prompt(
+        "Do you want to include LOWER CASE letters? (y or n)"
+      );
+      if (lowerCasePref === null) {
+        //if you cancel, stop prompting
+        return;
       }
-    } else {
-      alert("Enter y or n");
+      console.log(lowerCasePref + " lowercase");
+      if (validChecker(lowerCasePref)) {
+        // check valid input
+        validInput = true;
+        if (lowerCasePref == "y") {
+          pickPref = true; // true only when they picked a preference
+        }
+      } else {
+        alert("Enter y or n");
+      }
     }
+    // next prompt, next while loop
+    validInput = false;
+    while (!validInput) {
+      upperCasePref = prompt(
+        "Do you want to include UPPER CASE letters? (y or n)"
+      );
+      if (upperCasePref === null) {
+        return;
+      }
+      console.log(upperCasePref + " uppercase");
+      if (validChecker(upperCasePref)) {
+        validInput = true;
+        if (upperCasePref == "y") {
+          pickPref = true;
+        } // true only when they picked a preference
+      } else {
+        alert("Enter y or n");
+      }
+    }
+    // next prompt, next while loop
+    validInput = false;
+    while (!validInput) {
+      numericPref = prompt("Do you want to include NUMBERS? (y or n)");
+      if (numericPref === null) {
+        // returns if canceled.
+        return;
+      }
+      console.log(numericPref + " numeric");
+      if (validChecker(numericPref)) {
+        validInput = true;
+        if (numericPref == "y") {
+          pickPref = true;
+        } // true only when they picked a preference
+      } else {
+        alert("Enter y or n");
+      }
+    }
+    // next prompt, next while loop
+    validInput = false;
+    while (!validInput) {
+      specialPref = prompt(
+        "Do you want to include SPECIAL CHARACTERS? (y or n)"
+      );
+      if (specialPref === null) {
+        // returns if canceled.
+        return;
+      }
+      console.log(specialPref + " special chars");
+      if (validChecker(specialPref)) {
+        validInput = true;
+        if (specialPref == "y") {
+          pickPref = true;
+        } // true only when they picked a preference
+      } else {
+        alert("Enter y or n");
+      }
+    }
+    // console.log("WE MADE IT");
+    if (pickPref) {
+      return true;
+    }
+    // console.log("WHERES MY ALERT");
+    alert("You need to pick at least ONE character type preference!");
   }
-  // next prompt, next while loop
-  validInput = false;
-  while (!validInput) {
-    var upperCasePref = prompt(
-      "Do you want to include upper case letters? (y or n)"
-    );
-    if (upperCasePref === null) {
-      return;
-    }
-    console.log(upperCasePref + " uppercase");
-    if (validChecker(upperCasePref)) {
-      validInput = true;
-      if (lowerCasePref == "y") {
-        pickPref = true;
-      } // true only when they picked a preference
-    } else {
-      alert("Enter y or n");
-    }
-  }
-  // next prompt, next while loop
-  validInput = false;
-  while (!validInput) {
-    var numericPref = prompt("Do you want to include numbers? (y or n)");
-    if (numericPref === null) {
-      // returns if canceled.
-      return;
-    }
-    console.log(numericPref + " numeric");
-    if (validChecker(numericPref)) {
-      validInput = true;
-      if (lowerCasePref == "y") {
-        pickPref = true;
-      } // true only when they picked a preference
-    } else {
-      alert("Enter y or n");
-    }
-  }
+  // passed all validity and prompt checks. now pass on info write the generator
+  // console.log("WE MADE IT");
+  // if (pickPref) {
+  //   return true;
+  // }
+  // console.log("WHERES MY ALERT");
+  // alert("You need to pick at least ONE character type preference!");
 }
 
 // function to check validity of length; or yes or no prompts.
